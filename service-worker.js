68dce54fb8270c346bd4ded0ea03eaa33892ff89
okayama <kondoh@alfasado.jp>
2019-05-17 19:58:46 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,50 @@
+// configuration
+const
+  version = '1.0.0',
+  CACHE = version + '::PWAsite',
+  offlineURL = '/offline/index.html',
+  installFilesEssential = [
+    '/',
+    '/pwa-sample/manifest.json',
+    '/pwa-sample/styles.css',
+    '/pwa-sample/main.js',
+    '/pwa-sample/offline/index.html',
+    '/pwa-sample/offline/offline.css',
+    '/pwa-sample/offline/offline.js',
+    '/pwa-sample/pwa-logo.svg'
+  ].concat(offlineURL),
+  installFilesDesirable = [
+    '/p.html'
+  ];
+
+// install static assets
+function installStaticFiles() {
+
+  return caches.open(CACHE)
+    .then(cache => {
+
+      // cache desirable files
+      cache.addAll(installFilesDesirable);
+
+      // cache essential files
+      return cache.addAll(installFilesEssential);
+
+    });
+
+}
+
 // service-worker.js
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+// application installation
+self.addEventListener('install', event => {
+
+  console.log('service worker: install');
+
+  // cache core files
+  event.waitUntil(
+    installStaticFiles()
+    .then(() => self.skipWaiting())
+  );
+
 });
 
 self.addEventListener('activate', function(e) {
